8eba5efcf5e7b7f5f6df811ed35fcee1eb43b179
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-03-24 16:12:43 +0300

    Add_first_Javascript_lessons

---
+++ 01-WorkingWithVariables/I-)Let-Const/script.js
@@ -0,0 +1,22 @@
+var serverName = "patika.dev"
+console.log(serverName)
+
+
+let serverName2 = "kodluyoruz.org"
+console.log(serverName2)
+
+let fullName = "Ömer Faruk Birgül"
+console.log(fullName)
+
+fullName += " Osman Birgül"
+console.log(fullName)
+
+fullName += " yeni bilgi"
+fullName = "2.Bilgi " + fullName
+console.log(fullName)
+
+
+const password = 1234
+// password = 456  
+//const değişkeni bir kez tanımlandıktan sonra DEĞİŞTİRİLEMEZ!
+console.log(password)
