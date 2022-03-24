c34246b3eceeae62b948e6dea44c073f51935cf9
Omer Faruk Birgul <omerbirgul1@hotmail.com>
2022-03-24 17:05:36 +0300

    Add_NumberJavaScript_lessons

---
+++ 01-WorkingWithVariables/II-)Numbers_ArtihmeticOperations/script.js
@@ -0,0 +1,30 @@
+let price = 100
+let tax = 0.18
+let priceTax = price * tax
+let salePrice = priceTax + price
+console.log(salePrice)
+
+
+let stringnumber = "40"
+console.log(stringnumber)
+
+let newNumber =Number(stringnumber)
+console.log(newNumber)
+console.log("Number Constructor icine bilgi gonderildi.",Number("11"))
+
+
+let counter = 300
+counter++
+console.log(counter)
+counter +=1
+console.log(counter)
+
+
+//asagi yuvarlama islemi -> Math.floor:
+console.log(Math.floor(10,3))
+
+//yukari yuvarlama islemi -> Math.ceil:
+console.log(Math.ceil(7.2))
+
+//yakina yuvarlama islei -> Math.round:
+console.log(Math.round(1.3))
